aee95f73ab29d644a7390cf99e5daf9b1346c3bb
nyusternie <nyusternie@sdot.io>
2020-02-10 23:44:30 -0500

    Initial frameworking of AdminLTE.

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,18 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <AdminLTE />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+/* Import components. */
+import AdminLTE from './components/AdminLTE.vue'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+    components: {
+        AdminLTE
+    }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+/*  */
 </style>
