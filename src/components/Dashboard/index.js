a2bc3bd43a745cf45d9156ab6eaebccb3b0210fd
nyusternie <nyusternie@sdot.io>
2020-02-24 05:06:59 -0500

    Added an inline calendar to Dashboard.

--- src/components/Dashboard/index.js
+++ src/components/Dashboard/index.js
@@ -1,10 +1,12 @@
 /* Import components. */
+import Calendar from './Calendar'
 import GoalSummary from './GoalSummary'
 import InfoBoxes from './InfoBoxes'
 import TrendView from './TrendView'
 
 /* Export components. */
 export {
+    Calendar,
     GoalSummary,
     InfoBoxes,
     TrendView,
