cd4a763a49dc3f50ea957360f44b5caf6456c885
nyusternie <nyusternie@sdot.io>
2020-02-25 13:20:12 -0500

    Completed updates dashboard.

--- src/components/Dashboard/Updates.vue
+++ src/components/Dashboard/Updates.vue
@@ -21,61 +21,26 @@
                         <tr>
                             <th>Commit #</th>
                             <th>Project Name</th>
-                            <th>Status</th>
-                            <th>Activity</th>
+                            <th class="text-center">Build Status</th>
+                            <th class="text-center">Activity</th>
                         </tr>
                     </thead>
                     <tbody>
-                        <tr>
-                            <td><a href="javascript://">OR9842</a></td>
-                            <td>Call of Duty IV</td>
-                            <td><span class="badge badge-success">Shipped</span></td>
+                        <tr v-for="update of updates" v-bind:key="update.hashId">
                             <td>
-                                <div class="sparkline">6, 4, 10, 19, 16, 5, 18, 13, 20, 17, 8, 2, 12, 3, 9</div>
+                                <a :href="update.url" target="_blank">{{update.hashId}}</a>
+                            </td>
+                            <td>
+                                {{update.title}}
+                                <div class="description">{{update.description}}</div>
+                            </td>
+                            <td class="text-center">
+                                <span class="badge" :class="update.badge">{{update.status}}</span>
+                            </td>
+                            <td class="text-center">
+                                <div class="sparkline">{{update.sparkline}}</div>
                             </td>
                         </tr>
-                        <tr>
-                            <td><a href="javascript://">OR1848</a></td>
-                            <td>Samsung Smart TV</td>
-                            <td><span class="badge badge-warning">Pending</span></td>
-                            <td>
-                                <div class="sparkline">1, 15, 9, 12, 8, 20, 19, 5, 4, 13, 3, 6, 16, 14, 11</div>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td><a href="javascript://">OR7429</a></td>
-                            <td>iPhone 6 Plus</td>
-                            <td><span class="badge badge-danger">Delivered</span></td>
-                            <td>
-                                <div class="sparkline">8, 12, 10, 9, 16, 19, 1, 4, 5, 14, 6, 7, 18, 13, 11</div>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td><a href="javascript://">OR7429</a></td>
-                            <td>Samsung Smart TV</td>
-                            <td><span class="badge badge-info">Processing</span></td>
-                            <td>
-                                <span class="sparkline">15, 19, 10, 14, 4, 12, 8, 17, 3, 6, 2, 11, 1, 16, 20</span>
-                            </td>
-                        </tr>
-
-                        <tr>
-                            <td>
-                                <a href="https://gitlab.com/GeneralProtocols/electrum-cash/-/commit/5158a5bcf7218cd39f6c08e0069fa1434756b9f6" target="_blank">5158a5bc</a>
-                            </td>
-                            <td>
-                                electrum-cash
-                            </td>
-                            <td>
-                                <span class="badge badge-warning">Pending</span>
-                            </td>
-                            <td>
-                                <div>
-                                    <span class="sparkline">12, 17, 15, 2, 16, 5, 18, 9, 1, 6, 7, 3, 4, 20, 14</span>
-                                </div>
-                            </td>
-                        </tr>
-
                     </tbody>
                 </table>
             </div>
@@ -107,6 +72,65 @@
 
 <script>
 export default {
+    data: () => {
+        return {
+            updates: [],
+        }
+    },
+    computed: {
+        //
+    },
+    created: function () {
+        this.updates.push({
+            hashId: '9627b660',
+            url: 'https://gitlab.com/bitcoin-cash-node/bitcoin-cash-node/-/commit/9627b66018a73ccef7419f964fb3b646e03a71d1',
+            title: 'Bitcoin Cash Node',
+            description: `Merge branch 'rebrand_useragent_and_config' into 'master'`,
+            status: 'Errors',
+            badge: 'badge-danger',
+            sparkline: '6, 4, 10, 19, 16, 5, 18, 13, 20, 17, 8, 2, 12, 3, 9',
+        })
+
+        this.updates.push({
+            hashId: '581d205e',
+            url: 'https://github.com/BitcoinUnlimited/BitcoinUnlimited/commit/581d205e4e254be4a9f3f80c95e140447002e458',
+            title: 'BitcoinUnlimited',
+            description: `Merge pull request #2015 from sickpig/fix-build-icon`,
+            status: 'Building',
+            badge: 'badge-success',
+            sparkline: '8, 12, 10, 9, 16, 19, 1, 4, 5, 14, 6, 7, 18, 13, 11',
+        })
+
+        this.updates.push({
+            hashId: '1507ec53',
+            url: 'https://gitlab.com/pokkst/crescentcash/-/commit/1507ec53347117185100af1bd21539ad74a06a9c',
+            title: 'Crescent Cash',
+            description: `Update build.gradle`,
+            status: 'Building',
+            badge: 'badge-success',
+            sparkline: '1, 15, 9, 12, 8, 20, 19, 5, 4, 13, 3, 6, 16, 14, 11',
+        })
+
+        this.updates.push({
+            hashId: 'e4a7f10e',
+            url: 'https://gitlab.com/cashid/protocol-specification/-/commit/e4a7f10efe1da1efc2f7a5a84029c142c3ab11f8',
+            title: 'CashID Protocol Specification',
+            status: 'Pending',
+            description: `Update name for the known MITM risk.`,
+            badge: 'badge-warning',
+            sparkline: '15, 19, 10, 14, 4, 12, 8, 17, 3, 6, 2, 11, 1, 16, 20',
+        })
+
+        this.updates.push({
+            hashId: '5158a5bc',
+            url: 'https://gitlab.com/GeneralProtocols/electrum-cash/-/commit/5158a5bcf7218cd39f6c08e0069fa1434756b9f6',
+            title: 'electrum-cash',
+            description: `Fix linting.`,
+            status: 'Pending',
+            badge: 'badge-warning',
+            sparkline: '12, 17, 15, 2, 16, 5, 18, 9, 1, 6, 7, 3, 4, 20, 14',
+        })
+    },
     mounted: function () {
         /* Set JQuery. */
         const $ = window.$
@@ -115,13 +139,28 @@ export default {
         //     width: '100'
         // })
 
-        $('.sparkline').peity('line', {
-            width: '100'
-        })
+        /* Wait a tick for DOM to initialize. */
+        setTimeout(() => {
+            $('.sparkline').peity('line', {
+                width: '100'
+            })
+        }, 10)
+
     },
 }
 </script>
 
 <style scoped>
-/*  */
+.description {
+    font-size: 0.7em;
+    color: rgba(30, 30, 30, 0.5);
+
+    width: 300px;
+    white-space: nowrap;
+    overflow: hidden;
+    text-overflow: ellipsis;
+
+    margin-bottom: -10px;
+    /* padding-bottom: 0px; */
+}
 </style>
